4b53c66cd38ada91f0f948c73e169dbebc9d8c8c
JeanFelipe <ermelindo35@hotmail.com>
2023-08-22 10:58:54 -0300

    variaveis

---
+++ Aulas JavaScript/Aula02 variaveis/script.js
@@ -0,0 +1,28 @@
+// tipos primitivos
+
+//bolean 
+var vOuF = false;
+console.log(typeof (vOuF));
+
+//number 
+var number = 1;
+console.log(typeof (number));
+
+//string 
+var nome = 'jean';
+console.log(typeof (nome));
+
+//como declarar
+
+var variavel = 'Diana';
+variavel = 'Jean';
+
+console.log(variavel);
+
+
+let variavel2 = 4;
+variavel2 = 6
+console.log(variavel2);
+
+const variavel3 = 'Mario';
+console.log(variavel3);
