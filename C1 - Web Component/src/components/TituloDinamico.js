55b1b06c320d3898b52e1d361ec1fb81074baaf7
JeanFelipe <ermelindo35@hotmail.com>
2023-09-13 11:15:44 -0300

    Estilizando componente

---
+++ C1 - Web Component/src/components/TituloDinamico.js
@@ -0,0 +1,25 @@
+class TituloDinamico extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+
+        //base do componente
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = "Felipe";
+
+        //estilizar componente
+        const style = document.createElement("style");
+        style.textContent = `h1{
+            color: blue;
+        }`;
+        
+
+        //enviar para shadow
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define("titulo-dinamico", TituloDinamico);
